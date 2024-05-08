84bbaa61ca2ad025977bdc6b578d6a740c914301
Ivo Georgiev <ivogeorg@gmail.com>
2024-05-08 22:37:06 +0000

    removed /odom sub

--- checkpoint2.md
+++ checkpoint2.md
@@ -9,7 +9,7 @@ Creates a node providing a service `/rotate_robot` to rotate the RB1 robot in pl
 1. The node:
    1. Will be called `rotate_service_node`.
    2. Will advertise a service called `/rotate_robot`. It will be called from the command line, not through a programmatic client.
-   3. Will subscribe to topic `/odom`. The robot is rotated _in its own frame_, so this topic will be used to read its current position to rotate the robot the specified number of degrees. Note _degrees_ not _radians_.
+   3. ~Will subscribe to topic `/odom`.~ Does not have to subscribe to `/odom` because it gets the transform between the `/odom` and `base_footlink` frames from [`tf2_ros::Buffer::lookupTransform`](https://docs.ros.org/en/jade/api/tf2_ros/html/c++/classtf2__ros_1_1Buffer.html#acabbd72cae8f49fb3b6ede3be7e34c55). The robot is rotated _in its own frame_, so this topic will be used to read its current position to rotate the robot the specified number of degrees. Note that the service request message contains _degrees_ but the actual calculation is in _radians_.
    4. Will publish to topic `/cmd_vel`. The topic takes `geometry_msgs::Twist` messages.
    5. Will work with a custom service message `Rotate.srv`.
       ```
--- my_rb1_ros/src/rotate_service.cpp
+++ my_rb1_ros/src/rotate_service.cpp
@@ -7,11 +7,13 @@
 #include <ros/ros.h>
 #include <tf2_ros/transform_listener.h>
 
+#define __PI 3.14159265359
+
 class RB1RotateService {
 private:
   // ROS Objects
   ros::NodeHandle __nh;
-  tf2_ros::Buffer __tf_buf;
+  ros::Rate __rate;
 
   // ROS Services
   ros::ServiceServer __rot_svc;
@@ -20,45 +22,53 @@ private:
   ros::Publisher __vel_pub;
 
   // ROS Subscribers
-  ros::Subscriber __odom_sub;
-  tf2_ros::TransformListener __tf_listen;
+  // NOTE: Not necessary to subscribe to /odom.
+  //       tf2_ros::Buffer::lookupTransform() returns
+  //       tf b/n the /odom and base_footprint frames.
+  //   ros::Subscriber __odom_sub;
 
   // ROS Messages
   geometry_msgs::Twist __vel_msg;
   geometry_msgs::TransformStamped __tf_stamp;
 
+  // TF2
+  tf2_ros::Buffer __tf_buf;
+  tf2_ros::TransformListener __tf_listen;
+
+  // Other
+  float __angular_tolerance;
+
 public:
   RB1RotateService()
-      : {__rot_svc(__nh.advertiseService(
-             "/rotate_robot", &RB1RotateService::serviceCallback, this)),
-         __vel_pub(__nh.advertise<geometry_msgs::Twist>("/cmd_vel", 1)),
-         __odom_sub(__nh.subscribe("/odom", 1000, )),
-         __tf_listen(__tf_buf) {
+      : __rate(10.0),
+        __rot_svc(__nh.advertiseService(
+            "/rotate_robot", &RB1RotateService::serviceCallback, this)),
+        __vel_pub(__nh.advertise<geometry_msgs::Twist>("/cmd_vel", 1)),
+        __tf_listen(__tf_buf), __angular_tolerance(deg_to_rad(2)) {
     ROS_INFO("Service /rotate_robot: READY");
-}
+  }
 
-~RB1RotateService() {
-}
+  ~RB1RotateService() {}
 
-bool serviceCallback(my_rb1_ros::Rotate::Request &req,
-                     my_rb1_ros::Rotate::Response &res) {
-  // TODO
-}
+  // NOTE: Public functions in camel_case.
 
-void odometryCallback(const nav_msgs::OdometryConstPtr &odom_msg) {
-  // TODO
-}
+  bool serviceCallback(my_rb1_ros::Rotate::Request &req,
+                       my_rb1_ros::Rotate::Response &res) {
+    // TODO
+  }
 
 private:
-void get_odom() {
-  // TODO
-}
+  // NOTE: Private functions in snake_case.
+  void get_odom() {
+    // TODO: It's not void
+  }
 
-void rotate(int32 degrees) {
-  // TODO
-}
-}
-;
+  void rotate(int32 degrees) {
+    // TODO
+  }
+
+  float deg_to_rad(float deg) { return deg * __PI / 180.0; }
+};
 
 int main(int argc, char **argv) {
   ros::init(argc, argv, "rotate_service_node");
